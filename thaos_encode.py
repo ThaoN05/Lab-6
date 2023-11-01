142f9e140a3cf12c0b128bef708fca022a0ace62
ThaoN05 <imthethao@gmail.com>
2023-10-31 21:35:34 -0400

    encode function

--- thaos_encode.py
+++ thaos_encode.py
@@ -5,6 +5,12 @@ def encode(password):
     return encoded_password
 
 
+def decode(encoded_password):
+    # sofi's decode function
+    decoded_password = ''.join([str((int(digit) + 7) % 10) for digit in encoded_password])
+    return decoded_password
+
+
 def main():
     while True:
         # Display menu
@@ -12,7 +18,7 @@ def main():
         print("-------------")
         print("1. Encode")
         print("2. Decode")
-        print("2. Quit")
+        print("3. Quit")
 
         # Get user input
         choice = input("Please enter an option: ")
@@ -25,8 +31,12 @@ def main():
                 print("Your password has been encoded and stored!")
             except ValueError as e:
                 print(e)
+
         elif choice == '2':
-            #add decode function
+            # add decode function and remove "pass"
+            decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
         elif choice == '3':
             # Quit program
             print("Exiting program.")
